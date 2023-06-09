819261afadd39af64774917694f8de7c081ddbba
Sammi Chen <sammic432@users.noreply.github.com>
2023-06-09 14:47:09 -0700

    dropdowns

--- JS/searchPage.js
+++ JS/searchPage.js
@@ -1,15 +1,27 @@
-
+"use strict";
 
 fetch('http://localhost:8081/api/categories')
   .then(response => response.json())
   .then(data => {
     data.forEach(data => {
-       console.log(data.name); //To check our data display
-
+      // fills 2nd dropdown
+      console.log(data);
+      var element = document.createElement("option");
+      element.textContent = data.name;
+      categoryDropdown.appendChild(element);
     });
   });
 
-  window.onload = function(){
-    let searchButton = document.getElementById("searchBtn");
-    // searchButton.addEventListener("click", function(){ //input function })
+
+  let searchButton = document.getElementById("searchBtn");
+  const firstDropdown = document.getElementById("searchBy");
+  const categoryDropdown = document.getElementById("searchCategory");
+
+  //displays the 2nd dropdown menu if "search by category" is selected
+  function showSecondDropdown(el){
+    if (el.options[el.selectedIndex].value == 1){
+      categoryDropdown.style.display = 'block';
+    }else{
+      categoryDropdown.style.display = 'none';
+    }
   }
